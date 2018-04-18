f32e02ad6fe65e398c843776b9815c47d9661a42
derezzed78 <derezzed.78@gmail.com>
2018-04-18 15:12:08 +0800

    Structured folder and added react-native-facebook-login module

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import { FBLogin, FBLoginManager } from 'react-native-facebook-login'
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -19,20 +21,11 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <FBLogin />
       </View>
     );
   }
